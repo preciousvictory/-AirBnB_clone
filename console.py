eb9ff450f67d18c891e18626c85b4353b7df6ed1
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-02-08 06:07:37 -0800

    AirBnB clone project

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """
 A program that contains the entry point of the command interpreter
 """
+import cmd
 
 
 class HBNBCommand(cmd.Cmd):
@@ -9,3 +10,27 @@ class HBNBCommand(cmd.Cmd):
     Module defines the Cmd class
     Creating a command line interpreter is done by sub-classing the cmd.Cmd class.
     """
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """The quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """The  EOF (end of file) command to exit the program"""
+        return True
+
+    def emptyline(self):
+        """an empty line + ENTER shouldn’t execute anything"""
+        pass
+
+    def help_quit(self):
+        """help documentation for quit command"""
+        print("Quit command to exit the program\n")
+
+    def help_EOF(self):
+        """help documentation for EOF command"""
+        print("EOF command to exit the program\n")
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
