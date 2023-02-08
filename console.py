17485240388dcca74572def8758e4a5d4b9ff790
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-02-08 05:38:48 -0800

    AirBnB clone project

---
+++ console.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A program that contains the entry point of the command interpreter
+"""
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    Module defines the Cmd class
+    Creating a command line interpreter is done by sub-classing the cmd.Cmd class.
+    """
